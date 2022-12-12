c0a8b1066a7e363daf93a0c712f18904d34753aa
Morris Chan <morrismanfung@gmail.com>
2022-12-11 16:43:53 -0800

    Added workflow for model testing.

--- 02-model/04-svc-test.py
+++ 02-model/04-svc-test.py
@@ -18,35 +18,44 @@ import pickle
 import os
 
 from functions import *
+from classes import SVC_thld
 
 def main():
     df_train = pd.read_csv( '01-data/train.csv')
     df_test = pd.read_csv( '01-data/test.csv')
     X_train, y_train = df_train.drop( 'Winner', axis = 1), df_train[ 'Winner']
     X_test, y_test = df_test.drop( 'Winner', axis = 1), df_test[ 'Winner']
-
-    pipe_svc_opt = load( '02-model/01-saved-model/02-pipe_svc_opt.joblib')
+    column_transformer = load( '02-model/column_transformer.joblib')
 
     with open( '02-model/02-saved-scores/02-svc_dict_tmp.pkl', 'rb') as f:
         svc_dict = pickle.load( f)
+    best_params = svc_dict[ 'best_params']
 
     thld = float( pd.read_csv( '02-model/thresholds_used.csv', index_col = 0).loc[ 'SVC'])
+
+    pipe_svc_opt = final_svc( column_transformer, best_params, thld)
     svc_dict[ 'test_scores'] = model_testing( pipe_svc_opt, X_train, y_train, X_test, y_test, thld)
 
     dump( svc_dict, '02-model/02-saved-scores/02-svc_dict.joblib')
     with open( '02-model/02-saved-scores/02-svc_dict.pkl', 'wb') as f:
         pickle.dump( svc_dict, f)
 
-def svc_by_proba( pipe_svc, X_test, threshold):
-    proba = pipe_svc.decision_function( X_test)
-    y_hat = proba > threshold
-    return y_hat
+def final_svc( column_transformer, best_params, thld):
+    pipe_svc_opt = make_pipeline( column_transformer,
+                              SVC_thld( gamma = best_params[ 'svc_thld__gamma'],
+                                   C = best_params[ 'svc_thld__C'],
+                                   threshold = thld,
+                                   random_state = 918))
+
+    return pipe_svc_opt
 
 def model_testing( pipe_svc_opt, X_train, y_train, X_test, y_test, thld):
     pipe_svc_opt.fit( X_train, y_train)
-    y_hat_svc_opt = svc_by_proba( pipe_svc_opt, X_test, thld)
+    y_hat_svc_opt = pipe_svc_opt.predict( X_test)
+
     confusion_matrix_ = better_confusion_matrix( y_test, y_hat_svc_opt, labels = [ True, False])
     confusion_matrix_.to_csv( '02-model/02-saved-scores/02-svc_confusion_matrix.csv')
+
     classification_report_ = pd.DataFrame( classification_report( y_test, y_hat_svc_opt, output_dict = True))
     classification_report_.to_csv( '02-model/02-saved-scores/02-svc_classification_report.csv')
     return test_scoring_metrics( y_test, y_hat_svc_opt)
