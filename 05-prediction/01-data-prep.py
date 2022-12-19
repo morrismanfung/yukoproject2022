a0507a76bab921f8c4ea09f3bd202d673f5d474d
Morris Chan <morrismanfung@gmail.com>
2022-12-19 19:13:18 +0800

    Scripts for prediction.

---
+++ 05-prediction/01-data-prep.py
@@ -0,0 +1,169 @@
+# Author: Morris M. F. Chan
+# Date: 2022-12-07
+
+'''
+This script takes the raw data for prediction and transform them into the form our models can read.
+
+Usage:
+01-data-prep.py --race_date=<race_date>
+
+Options:
+--race_date=<race_date>     Date of the race.
+
+Example:
+python 05-prediction/01-data-prep.py --race_date='2022-12-07'
+'''
+
+import numpy as np
+import pandas as pd
+from docopt import docopt
+
+opt = docopt(__doc__)
+
+def main( race_date):
+    df_race = pd.read_csv( '../05-prediction/00-future-input.csv')
+    df_horse = pd.read_csv( '../01-data/basic-horse.csv') # Retired horse is not needed becuase only current horses are in the race.
+    df_jockey = pd.read_csv( '../01-data/basic-jockey.csv')
+    df_trainer = pd.read_csv( '../01-data/basic-trainer.csv')
+
+    df_race_tr = df_race.loc[ :, ~ df_race.columns.str.startswith( 'G_')]
+
+    df_horse = df_horse.iloc[ :, 1:]
+
+    columns_horse = [ 'HorseName', 'HorseOrigin', 'Age', 'YearOfBirth', 'Color', 'Sex', 'H_SeasonalStake', 'H_TotalStake', 'H_1st', 'H_2nd', 'H_3rd', 'H_Total', 'H_Trainer', 'CurrentRating', 'StartofTheSeasonRating']
+    df_horse.columns = columns_horse
+    df_horse[ 'HorseName'] = df_horse[ 'HorseName'].str.replace( ' ', '', regex = False)
+    df_horse[ 'HorseName'] = df_horse[ 'HorseName'].str.replace( r'\(.*\)', '', regex = True)
+    df_horse[ 'H_SeasonalStake'] = df_horse[ 'H_SeasonalStake'].str.replace( '$', '', regex = False).str.replace( ',', '').astype( 'int')
+    df_horse[ 'H_TotalStake'] = df_horse[ 'H_TotalStake'].str.replace( '$', '', regex = False).str.replace( ',', '').astype( 'int')
+    df_horse[ 'H_Rate_win'] = df_horse[ 'H_1st'] / df_horse[ 'H_Total']
+    df_horse[ 'H_Rate_place'] = df_horse[ [ 'H_1st', 'H_2nd']].sum( axis = 1) / df_horse[ 'H_Total']
+    df_horse[ 'H_Rate_show'] = df_horse[ [ 'H_1st', 'H_2nd', 'H_3rd']].sum( axis = 1) / df_horse[ 'H_Total']
+
+    df_jockey = df_jockey.iloc[ :, 1:]
+    columns_jockey = [ 'JockeyName', 'J_1st', 'J_2nd', 'J_3rd', 'J_4th', 'J_5th', 'J_Total', 'J_TotalStakes']
+    df_jockey.columns = columns_jockey
+    df_jockey[ 'J_ratio_1st'] = df_jockey[ 'J_1st'] / df_jockey[ 'J_Total']
+    df_jockey[ 'J_ratio_Top5'] = df_jockey[ [ 'J_1st', 'J_2nd', 'J_3rd', 'J_4th', 'J_5th']].sum( axis = 1) / df_jockey[ 'J_Total']
+    df_jockey[ 'J_TotalStakes'] = df_jockey[ 'J_TotalStakes'].str.replace( '$', '', regex = False).str.replace( ',', '').astype( 'int')
+    df_jockey[ 'J_Rate_win'] = df_jockey[ 'J_1st'] / df_jockey[ 'J_Total']
+    df_jockey[ 'J_Rate_place'] = df_jockey[ ['J_1st', 'J_2nd']].sum( axis = 1) / df_jockey[ 'J_Total']
+    df_jockey[ 'J_Rate_show'] = df_jockey[ ['J_1st', 'J_2nd', 'J_3rd']].sum( axis = 1) / df_jockey[ 'J_Total']
+
+    df_trainer = df_trainer.iloc[ :, 1:]
+    columns_trainer = [ 'TrainerName', 'T_1st', 'T_2nd', 'T_3rd', 'T_4th', 'T_5th', 'T_Total', 'T_TotalStakes']
+    df_trainer.columns = columns_trainer
+    df_trainer[ 'T_ratio_1st'] = df_trainer[ 'T_1st'] / df_trainer[ 'T_Total']
+    df_trainer[ 'T_ratio_Top5'] = df_trainer[ [ 'T_1st', 'T_2nd', 'T_3rd', 'T_4th', 'T_5th']].sum( axis = 1) / df_trainer[ 'T_Total']
+    df_trainer[ 'T_TotalStakes'] = df_trainer[ 'T_TotalStakes'].str.replace( '$', '', regex = False).str.replace( ',', '').astype( 'int')
+    df_trainer[ 'T_Rate_win'] = df_trainer[ 'T_1st'] / df_trainer[ 'T_Total']
+    df_trainer[ 'T_Rate_place'] = df_trainer[ [ 'T_1st', 'T_2nd']].sum( axis = 1) / df_trainer[ 'T_Total']
+    df_trainer[ 'T_Rate_show'] = df_trainer[ [ 'T_1st', 'T_2nd', 'T_3rd']].sum( axis = 1) / df_trainer[ 'T_Total']
+
+    df_merged_j = pd.merge( df_race_tr, df_jockey, left_on = 'Jockey', right_on = 'JockeyName', how = 'left')
+    df_merged_jt = pd.merge( df_merged_j, df_trainer, left_on = 'Trainer', right_on = 'TrainerName', how = 'left')
+    df_merged_jth = pd.merge( df_merged_jt, df_horse, left_on = 'HorseName', right_on = 'HorseName', how = 'left')
+    df_merged_jth[ 'H_Age'] = df_merged_jth[ 'RaceYear'] - df_merged_jth[ 'YearOfBirth']
+    df_merged_jth[ 'CurrentRating'] = df_merged_jth[ 'RatingN']
+
+    df_merged_jth[ 'Previous_list'] = df_merged_jth['Previous'].str.split('/')
+
+    df_merged_jth[ 'Previous_list'] = df_merged_jth[ 'Previous_list'].map( to_int)
+
+    df_merged_jth[ 'PreviousPlace'] = df_merged_jth[ 'Previous_list'].map( previous)
+
+    df_merged_jth[ 'AvgPlace3'] = df_merged_jth[ 'Previous_list'].map( avg3)
+    df_merged_jth[ 'AvgPlace5'] = df_merged_jth[ 'Previous_list'].map( avg5)
+
+    df_merged_jth[ 'LastRace_nDays'] = pd.Series( dtype = 'float64')
+    df_merged_jth[ 'WeightDiff'] = pd.Series( dtype = 'float64')
+
+    for i in range( len( df_merged_jth)):
+        try:
+            HorseName = df_merged_jth.loc[ i, 'HorseName']
+            DeclaredHorseWeight = df_merged_jth.loc[ i, 'DeclaredHorseWeight']
+            tmp_table = pd.read_csv( f'01-data/horse-race-history/{HorseName}.csv')
+            tmp_table = tmp_table[ ['RDate', 'LastRace_nDays', 'RPlace', 'PreviousPlace', 'AvgPlace3', 'AvgPlace5', 'RRating', 'Weight','WeightDiff']]
+            tmp_table[ 'RDate'] = pd.to_datetime( tmp_table[ 'RDate'], format = '%Y-%m-%d')
+        except:
+            df_merged_jth.loc[ i, 'LastRace_nDays'] = 100
+            df_merged_jth.loc[ i, 'WeightDiff'] = 0
+
+        try:
+            nDays = (pd.to_datetime( race_date, format = '%Y-%m-%d') - tmp_table.loc[ 0, 'RDate']).days
+            df_merged_jth.loc[ i, 'LastRace_nDays'] = nDays
+        except:
+            df_merged_jth.loc[ i, 'LastRace_nDays'] = 100
+        
+        try:
+            PreviousWeight = tmp_table.loc[ 0, 'Weight']
+            WeightDiff = DeclaredHorseWeight - PreviousWeight
+            df_merged_jth.loc[ i, 'WeightDiff'] = WeightDiff
+        except:
+            df_merged_jth.loc[ i, 'WeightDiff'] = 0
+
+
+    df_merged_jth[ 'CombinedRating'] = df_merged_jth[ 'RatingN']
+    df_merged_jth = df_merged_jth.fillna( 0)
+
+    X = df_merged_jth.loc[ :, df_merged_jth.columns.str.startswith( 'ActualWeight') | df_merged_jth.columns.str.startswith( 'DeclaredHorseWeight') | \
+        df_merged_jth.columns.str.startswith( 'WinOdds') | df_merged_jth.columns.str.startswith( 'Draw') | \
+        df_merged_jth.columns.str.startswith( 'J_Total_') | df_merged_jth.columns.str.startswith( 'J_TotalStakes_') | \
+        df_merged_jth.columns.str.startswith( 'J_Rate_') | \
+        df_merged_jth.columns.str.startswith( 'T_Total_') | df_merged_jth.columns.str.startswith( 'T_TotalStakes_') | \
+        df_merged_jth.columns.str.startswith( 'T_Rate') | \
+        df_merged_jth.columns.str.startswith( 'H_Rate_') | df_merged_jth.columns.str.startswith( 'H_Total') | \
+        df_merged_jth.columns.str.startswith( 'H_TotalStake') | df_merged_jth.columns.str.startswith( 'CombinedRating') | \
+        df_merged_jth.columns.str.startswith( 'H_Age') | df_merged_jth.columns.str.startswith( 'WeightDiff') | \
+        df_merged_jth.columns.str.startswith( 'LastRace_nDays') | df_merged_jth.columns.str.startswith( 'PreviousPlace') | \
+        df_merged_jth.columns.str.startswith( 'AvgPlace3') | df_merged_jth.columns.str.startswith( 'AvgPlace5') | \
+        df_merged_jth.columns.str.startswith( 'Place') | \
+        df_merged_jth.columns.str.startswith( 'Race')] # Place for downsampling
+
+    X_mean = X.groupby( 'Race').mean()
+    X_std = X.groupby( 'Race').std()
+
+    X_mean.columns = list( map( lambda x: '_'.join( [x, 'mean']), X_mean.columns))
+    X_std.columns = list( map( lambda x: '_'.join( [x, 'std']), X_std.columns))
+
+    X_mean_std = pd.concat( [X_mean, X_std], axis = 1)
+
+    X_norm = pd.merge( X, X_mean_std, left_on = 'Race', right_index = True, how = 'inner')
+    X_mean_whole = X_norm.loc[:, X_norm.columns.str.endswith( 'mean')]
+    X_std_whole = X_norm.loc[:, X_norm.columns.str.endswith( 'std')]
+    print( X.shape, X_mean_whole.shape, X_std_whole.shape)
+
+    race_vector = X[ 'Race']
+    X = X.drop( 'Race', axis = 1)
+
+    X_norm = pd.DataFrame( ( np.asarray( X) - np.asarray( X_mean_whole)) / np.asarray( X_std_whole))
+    X_norm = X_norm.fillna( 0)
+    X_norm.columns = list( map( lambda x: '_'.join( [x, 'norm']), X.columns))
+    X_comb = pd.concat( [ df_merged_jth, X_norm], axis = 1)
+
+    X_comb.to_csv( '../05-prediction/01-preprocessed_testing.csv', encoding = 'utf-8_sig')
+
+
+def to_int( x):
+    try:
+        return list( map( int, x))
+    except:
+        return [ 10]
+
+def avg3( history):
+    try:
+        return np.mean( history[ 0:4])
+    except:
+        return np.mean( history)
+
+def avg5( history):
+    try:
+        return np.mean( history[ 0:6])
+    except:
+        return np.mean( history)
+
+def previous( history):
+    return history[ 0]
+
+if __name__ == "__main__":
+  main( opt[ '--race_date'])
---
+++ 05-prediction/02-prediction.py
@@ -0,0 +1,100 @@
+# Author: Morris M. F. Chan
+# 2022-12-19
+# Usage: python 05-prediction/02-prediction.py
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import cross_val_score, cross_validate, train_test_split, GridSearchCV, RandomizedSearchCV
+from sklearn.preprocessing import StandardScaler, FunctionTransformer
+from sklearn.pipeline import Pipeline, make_pipeline
+from sklearn.compose import ColumnTransformer, make_column_transformer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, confusion_matrix
+from joblib import load
+import pickle
+
+import sys
+sys.path.append('05-prediction/')
+from classes import *
+
+def main():
+    pipe_rfc_opt, pipe_logreg_opt, pipe_lsvc_opt = model_training()
+    future_input = pd.read_csv( '05-prediction/01-preprocessed_20221130.csv')
+    y_hat_rfc = pipe_rfc_opt.predict( future_input)
+    y_hat_logreg = pipe_logreg_opt.predict( future_input)
+    y_hat_lsvc = pipe_lsvc_opt.predict( future_input)
+    vote = y_hat_rfc.astype( 'int') + y_hat_logreg.astype( 'int') + y_hat_lsvc.astype( 'int')
+    y_hat_vote = vote > 1
+    y_hat_any = vote > 0
+
+    future_input[ 'rfc'] = y_hat_rfc
+    future_input[ 'logreg'] = y_hat_logreg
+    future_input[ 'lsvc'] = y_hat_lsvc
+    future_input[ 'vote'] = y_hat_vote
+    future_input[ 'any'] = y_hat_any
+    future_input[ ['Race', 'HorseNo', 'HorseName', 'WinOdds', 'rfc', 'logreg', 'lsvc', 'vote', 'any']].to_csv( '05-prediction/03-result.csv', encoding = 'utf-8_sig')
+
+def model_training():
+    df_train = pd.read_csv( '01-data/train.csv')
+    df_test = pd.read_csv( '01-data/test.csv')
+    X_train, y_train = df_train.drop( 'Winner', axis = 1), df_train[ 'Winner']
+    X_test, y_test = df_test.drop( 'Winner', axis = 1), df_test[ 'Winner']
+
+    column_transformer = load( '02-model/column_transformer.joblib')
+    
+    with open( '02-model/02-saved-scores/03-rfc_dict_tmp.pkl', 'rb') as f:
+        rfc_dict = pickle.load( f)
+    best_params_rfc = rfc_dict[ 'best_params']
+    thld_rfc = float( pd.read_csv( '02-model/thresholds_used.csv', index_col = 0).loc[ 'RFC_pre'])
+
+    with open( '02-model/02-saved-scores/05-logreg_dict_tmp.pkl', 'rb') as f:
+        logreg_dict = pickle.load( f)
+    best_params_logreg = logreg_dict[ 'best_params']
+    thld_logreg = float( pd.read_csv( '02-model/thresholds_used.csv', index_col = 0).loc[ 'LogReg_pre'])
+
+    with open( '02-model/02-saved-scores/06-lsvc_dict_tmp.pkl', 'rb') as f:
+        lsvc_dict = pickle.load( f)
+    best_params_lsvc = lsvc_dict[ 'best_params']
+    thld_lsvc = float( pd.read_csv( '02-model/thresholds_used.csv', index_col = 0).loc[ 'LinearSVC_pre'])
+
+    pipe_rfc_opt = final_rfc( column_transformer, best_params_rfc, thld_rfc)
+    pipe_logreg_opt = final_logreg( column_transformer, best_params_logreg, thld_logreg)
+    pipe_lsvc_opt = final_lsvc( column_transformer, best_params_lsvc, thld_lsvc)
+
+    pipe_rfc_opt.fit( X_train, y_train)
+    pipe_logreg_opt.fit( X_train, y_train)
+    pipe_lsvc_opt.fit( X_train, y_train)
+
+    return pipe_rfc_opt, pipe_logreg_opt, pipe_lsvc_opt
+
+def final_rfc( column_transformer, best_params, thld):
+    pipe_rfc_opt = make_pipeline( column_transformer,
+                              RFC_thld( n_estimators = best_params[ 'rfc_thld__n_estimators'],
+                                        max_features = best_params[ 'rfc_thld__max_features'],
+                                        max_depth = best_params[ 'rfc_thld__max_depth'],
+                                        criterion = best_params[ 'rfc_thld__criterion'],
+                                        bootstrap = best_params[ 'rfc_thld__bootstrap'],
+                                        threshold = thld,
+                                        random_state = 918))
+    return pipe_rfc_opt
+
+def final_logreg( column_transformer, best_params, thld):
+    pipe_logreg_opt = make_pipeline( column_transformer,
+                                     LogReg_thld( C = best_params[ 'logreg_thld__C'],
+                                                  l1_ratio = best_params[ 'logreg_thld__l1_ratio'],
+                                                  threshold = thld,
+                                                  random_state = 918))
+    return pipe_logreg_opt
+
+def final_lsvc( column_transformer, best_params, thld):
+    pipe_lsvc_opt = make_pipeline( column_transformer,
+                                   LinearSVC_thld( C = best_params[ 'linearsvc_thld__C'],
+                                                   threshold = thld,
+                                                   random_state = 918))
+
+    return pipe_lsvc_opt
+
+if __name__ == '__main__':
+    main()
